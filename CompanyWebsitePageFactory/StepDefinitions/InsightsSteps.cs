be24e38a67d64a59ca70fff984215d99be1965e6
Murp01 <pmurphy35@hotmail.com>
2019-06-16 11:55:19 +0100

    I've added Cucumber feature and steps but I'm getting ahead of myself - not done anything with them yet

---
+++ CompanyWebsitePageFactory/StepDefinitions/InsightsSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace CompanyWebsitePageFactory.StepDefinitions
+{
+    [Binding]
+    public class InsightsSteps
+    {
+        [Given(@"I am on the Linklaters homepage")]
+        public void GivenIAmOnTheLinklatersHomepage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I click on Insights from the global navigation bar")]
+        public void GivenIClickOnInsightsFromTheGlobalNavigationBar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I enter a search term into Insights name search box")]
+        public void WhenIEnterASearchTermIntoInsightsNameSearchBox()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"all results containing the search box will appear")]
+        public void ThenAllResultsContainingTheSearchBoxWillAppear()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
